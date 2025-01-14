6db4f585b848d2c67e38880e5fbd3cc76debcdc0
rahul1015s <rahulvermakumar10@gmail.com>
2025-01-15 01:20:02 +0530

    feat: refactor Cart component to use CartItem for better item representation; enhance CartItem with increase and decrease functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,8 @@ import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem.jsx";
+
 export default function Cart() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
@@ -12,7 +14,7 @@ export default function Cart() {
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
-   
+
   function handleCloseCart() {
     userProgressCtx.hideCart();
   }
@@ -21,10 +23,15 @@ export default function Cart() {
     <Modal open={userProgressCtx.progress === "cart"}>
       <h2 className="font-alatsi text-2xl mb-4">Your Cart</h2>
       <ul>
-        {cartCtx.items.map((items) => (
-          <li  key={items.id}>
-            {items.name} - {items.quantity}
-          </li>
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="font-alatsi font-bold text-lg mb-6 flex justify-end ">
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,13 +1,18 @@
-import { currencyFormatter } from "../util/formatting.jsx"
-export default function CartItem ({ name, quantity, price}) {
-    return <li>
-        <p>
-            {name}- {quantity} X {currencyFormatter.format(price)}
-        </p>
-        <P>
-            <button>-</button>
-            <button>{quantity}</button>
-            <button>+</button>
-        </P>
+import { currencyFormatter } from "../util/formatting.jsx";
+
+export default function CartItem({ name, quantity, price, onIncrease, onDecrease }) {
+
+    
+  return (
+    <li className="flex justify-between items-center p-4 border-b">
+      <p className="text-gray-800">
+        {name} - {quantity} X {currencyFormatter.format(price)}
+      </p>
+      <div className="flex items-center space-x-2">
+        <button className="bg-slate-500 text-white rounded px-3 py-1 hover:bg-slate-600" onClick={onDecrease}>-</button>
+        <button className="px-3 py-1 bg-gray-200 rounded" >{quantity}</button>
+        <button className="bg-slate-500 text-white rounded px-3 py-1 hover:bg-slate-600" onClick={onIncrease}>+</button>
+      </div>
     </li>
+  );
 }
